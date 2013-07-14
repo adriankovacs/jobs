bb431b079f5a8125d412f48a4d4b1ddb44d89e45
Adrian Kovacs <az.adrian.kovacs@gmail.com>
2013-07-14 11:19:29 +0200

    Check input format

--- jobs.rb
+++ jobs.rb
@@ -9,16 +9,21 @@ class Jobs
 
   def self.get_job
     # Get the job or control commands
-    puts "Pleas give me a job. (Format: a => [b] #b is optional) Hit ':p' to PROCESS sequence, ':c' to CLEAR sequence or ':q' to EXIT."
+    puts "Please give me a job. (Format: a => [b] #b is optional) Hit ':p' to PROCESS sequence, ':c' to CLEAR sequence or ':q' to EXIT."
     self.process_job(gets.chomp)
   end
 
-  def self.check_job(job)
-    # Some tests on input format and dependencies.
+  def self.check_job_format(job)
+    # Some tests on input format.
+    if job.match(/^:[pcq]$/).nil? && job.match(/^[a-zA-Z] *=> *[a-zA-Z]?$/).nil?
+      puts "ERROR: Unknown job format or command character."
+      puts "Do you mean: \"#{job} =>\" ?" unless job.match(/^[a-zA-Z]$/).nil?
+      self.get_job
+    end
   end
 
   def self.process_job(job)
-    self.check_job(job)
+    self.check_job_format(job)
 
     # Control commands
     self.print_sequence   if job == ":p"
